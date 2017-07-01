a36dc9d6121ac93b9ebc20a7de1bfb3b4de456d5
Sandor David Olasz <olaszsandordavid@gmail.com>
2017-07-01 16:31:45 +0200

    Part 2

--- part2/export.py
+++ part2/export.py
@@ -1,2 +1,27 @@
+from reports import *
 
-# Export functions
+file = open('exports.txt', 'w')
+
+print("1. What is the title of the most played game (i.e. sold the most copies)?\n2. How many copies have been sold total?\n3. What is the average selling?\n" +
+        "4. How many characters long is the longest title?\n5. What is the average of the release dates?\n6. What properties has a game?")
+
+while True:
+    starter = input("Which of the above ones are you interested in? (Press the number or hit the q key to exit):\n")
+    if starter == '1':
+        file.write(get_most_played('game_stat.txt') + '\n')
+    if starter == '2':
+        file.write(str(sum_sold('game_stat.txt')) + '\n')
+    if starter == '3':
+        file.write("{:.2f}\n".format(get_selling_avg('game_stat.txt')))
+    if starter == '4':
+        file.write(str(count_longest_title('game_stat.txt')) + '\n')
+    if starter == '5':
+        file.write(str(get_date_avg('game_stat.txt')) + '\n')
+    if starter == '6':
+        file.write(str(get_game('game_stat.txt', input('Please name a game: '))) + '\n')
+    if starter == 'q':
+        break
+    else:
+        continue
+
+file.close()
--- part2/reports.py
+++ part2/reports.py
@@ -1,2 +1,68 @@
+import math
 
-# Report functions
+
+def get_most_played(file_name):
+    most_played = 0
+    most_played_game = ""
+    
+    with open(file_name) as file:
+        for line in file:
+            line = line.split("\t")
+            if float(line[1]) > most_played:
+                most_played = float(line[1])
+                most_played_game = line[0]
+    return most_played_game
+
+
+def sum_sold(file_name):
+    sold_copies = 0
+
+    with open(file_name) as file:
+        for line in file:
+            line = line.split("\t")
+            sold_copies += float(line[1])
+    return sold_copies
+
+
+def get_selling_avg(file_name):
+    sum_of_lines = 0
+
+    with open(file_name) as file:
+        for line in file:
+            sum_of_lines += 1
+
+    return sum_sold(file_name) / sum_of_lines
+
+
+def count_longest_title(file_name):
+    max_len = 0
+
+    with open(file_name) as file:
+        for line in file:
+            line = line.split("\t")
+            if len(line[0]) > max_len:
+                max_len = len(line[0])
+    return max_len
+
+
+def get_date_avg(file_name):
+    sum_of_years = 0
+    sum_of_lines = 0
+
+    with open(file_name) as file:
+        for line in file:
+            line = line.split("\t")
+            sum_of_years += int(line[2])
+            sum_of_lines += 1
+    return math.ceil(sum_of_years / sum_of_lines)
+
+
+def get_game(file_name, title):
+    with open(file_name) as file:
+        for line in file:
+            line = line.split("\t")
+            if line[0] == title:
+                line[1] = float(line[1])
+                line[2] = int(line[2])
+                line[4] = line[4].replace('\n', '')
+                return line
